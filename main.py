9d2def87fb1c7ae3536fb653cb47558de66c4829
2rootc <rmsckd0415@naver.com>
2023-02-10 00:41:00 +0900

    change file

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ dbcn = pyodbc.connect('DSN=JUDB')
 print("DB접속 완료")
 
 rows = pd.read_sql("""
-    SELECT DISTINCT ADMIN_ID, MEAS_DT FROM IST_METER_DATA
+    SELECT DISTINCT ADMIN_ID FROM IST_METER_DATA
 """, dbcn)
 
 
 df = pd.DataFrame(rows)
 
-# df.to_excel("test.xlsx",index=None)
+df.to_excel("test.xlsx",index=None)
 
 print(df)
 
